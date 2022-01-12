05303b6638fdee47ef64a7a52ef5999e46321901
zhivko.petrov <zhivko.petrov@ocado.com>
2022-01-12 11:52:27 +0200

    feat: add suport for install_and_export_target and enable_ros_tooling_for_target

---
+++ resource_utilsConfig.cmake
@@ -0,0 +1,14 @@
+include(CMakeFindDependencyMacro)
+
+#find_dependency will correctly forward REQUIRED or QUIET args to the consumer
+#find_package is only for internal use
+find_dependency(utils REQUIRED)
+
+if(NOT TARGET resource_utils::resource_utils)
+  include(${CMAKE_CURRENT_LIST_DIR}/resource_utilsTargets.cmake)
+endif()
+
+# This is for catkin compatibility.
+set(resource_utils_LIBRARIES resource_utils::resource_utils)
+get_target_property(resource_utils_INCLUDE_DIRS resource_utils::resource_utils INTERFACE_INCLUDE_DIRECTORIES)
+
